f7bcf4bd55e060efc9c8b3d7c57ca9e138d049a0
Alexey <vip.leontev2002@gmail.com>
2023-04-08 23:08:14 +0900

    add bar-chart and fix button

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,49 +1,10 @@
 import React, {FC} from 'react'
-import {Breadcrumbs, Calendar, Link, Map, Tab, Tabs, Text, Button, VideoPlayer, Alert, LineChart} from '../../share'
+import {Breadcrumbs, Calendar, Link, Map, Tab, Tabs, Text, Button, VideoPlayer, Alert, Chart} from '../../share'
 import {Path} from '../../app'
-
-const data = {
-    name: 'Любое имя диаграммы',
-    dataArray: [
-        {
-            label: 'месяц 1',
-            value: 13
-        },
-        {
-            label: 'месяц 2',
-            value: 24
-        },
-        {
-            label: 'месяц 3',
-            value: 17
-        },
-        {
-            label: 'месяц 4',
-            value: 19
-        },
-    ]
-}
-
-const name = 'Любое имя диаграммы'
-const labels = ['месяц 1', 'месяц 2', 'месяц 3', 'месяц 4', ]
-const datasets = [
-    {
-        label: 'Данные 1',
-        data: [13, 15, 26, 10],
-        borderColor: '#32a852',
-        backgroundColor: '#1c6b31'
-    },
-    {
-        label: 'Данные 1',
-        data: [19, 22, 20, 8],
-        borderColor: '#e012be',
-        backgroundColor: '#6b1c5e'
-    },
-]
 
 const HomePage: FC = () => (
     <div>
-        <LineChart name={name} labels={labels} datasets={datasets}/>
+    
         <Text $size="small">HomePage И что то на русском</Text>
         <Tabs>
             <Tab label="First">I think</Tab>
@@ -56,7 +17,7 @@ const HomePage: FC = () => (
         </Breadcrumbs>
         <Calendar/>
         <Map lat={113.5} lon={52.03} zoom={9}/>
-        <Button> dfasdasd asd as das </Button>
+        <Button > dfasdasd asd as das </Button>
         <VideoPlayer listSrc={['./']} />
         <Alert $variant='warning'>12312</Alert>
     </div>
