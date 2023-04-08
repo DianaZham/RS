a4dde92ce63119bf98651d4f8d4ddd4ffb91bcaa
Alexey <vip.leontev2002@gmail.com>
2023-04-08 17:53:04 +0900

    button ready

--- frontend/src/share/ui-kit/button/button.ts
+++ frontend/src/share/ui-kit/button/button.ts
@@ -1,15 +1,15 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-    height: 40px;
-    background: #0E91EF;
-	box-shadow: 0px 4px 8px rgba(0, 15, 115, 0.08);
+    height: ${({theme}) => theme.spacing['sp-7']}px;
+	background: ${({theme}) => theme.colors.primary.dark};
+	box-shadow: ${({theme}) => theme.shadow.normal};
 	border: none;
-	border-radius: 8px;
-	padding: 0px 24px;
-	font-weight: 700;
-	font-size: 14px;
-	color: #17181A;
+	border-radius:  ${({theme}) => theme.spacing['sp-2']}px;
+	padding: 0px ${({theme}) => theme.spacing['sp-5']}px;
+	font-weight: ${({theme}) => theme.font.desktop['button-large'].weight};
+	font-size:  ${({theme}) => theme.font.desktop['button-large'].size}px;
+	color: ${({theme}) => theme.colors.neutral.black};
 `
 
 export default Button
