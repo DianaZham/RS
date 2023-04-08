50163be7216fc4081d75ca6ea629ed15ac47c0c1
Alexey <vip.leontev2002@gmail.com>
2023-04-08 17:32:18 +0900

    button ready

---
+++ frontend/src/share/ui-kit/input/input.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Input = styled.input`
+    height: 40px;
+    background: #0E91EF;
+	box-shadow: 0px 4px 8px rgba(0, 15, 115, 0.08);
+	border: none;
+	border-radius: 8px;
+	padding: 0px 24px;
+	font-weight: 700;
+	font-size: 14px;
+	color: #17181A;
+`
+
+export default Input
---
+++ frontend/src/share/ui-kit/input/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Input = styled.input`
+    background: #FFFFFF;
+    border: 1px solid #E1E2E6;
+    border-radius: 8px;
+    height: 48px
+    padding: 0px 16px;
+    width: 100%;
+`
+
+export default Input
