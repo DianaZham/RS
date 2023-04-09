c0c1a83bfc2b5ff87448a6a5d79d8a7e280e8ac9
kirill <glad-web-developer@gmail.com>
2023-04-09 23:04:24 +0900

    докер

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('otcheti/', include('apps.otcheti.urls')),
+    path('api/otcheti/', include('apps.otcheti.urls')),
     path('', TemplateView.as_view(template_name='index.html'))
 ]
 if settings.DEBUG:
