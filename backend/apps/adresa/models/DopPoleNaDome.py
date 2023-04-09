13d324ed791d04602c42ed236ac925b0125b7ade
kirill <glad-web-developer@gmail.com>
2023-04-09 23:37:52 +0900

    xml

--- backend/apps/adresa/models/DopPoleNaDome.py
+++ backend/apps/adresa/models/DopPoleNaDome.py
@@ -11,6 +11,7 @@ class DopPoleNaDome(models.Model):
 
     dop_pole_doma = models.ForeignKey(DopPoleDoma, verbose_name='Доп поле дома', on_delete=models.CASCADE)
     dom = models.ForeignKey(Dom, verbose_name='Дом', on_delete=models.CASCADE)
+    znachenie = models.CharField('Значение', null=True, blank=True, max_length=255)
 
     def __str__(self):
         return f'{self.dom.get_full_adres()} {self.dop_pole_doma.name}'
